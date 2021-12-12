4ba42c7c02fd6facd6777366c89531c3bd7fcf63
鬼魂 <46194156+nzwh@users.noreply.github.com>
2021-12-12 23:51:27 +0800

    Implement playSequence function
    Coded the logic to generate the player sequence from 1 to 5 into a single integer.

--- functions.h
+++ functions.h
@@ -20,3 +20,34 @@ void printBoard(int length, int height) {
     }
 }
 
+int isUnique(int check, int a, int b, int c, int d, int e) {
+
+    if (check != a && check != b && check != c && check != d && check != e)
+        return 1;
+    else    
+        return 0;
+}
+
+int playerSequence(int limit) {
+
+    int digit = 0, temp = 0;
+    for (int i = 1; i <= limit; i++) {
+        temp = (rand()%limit)+1;
+        int exit = 0;
+        
+        if (digit == 0)
+            digit += temp * pow(10, i-1);
+        else if (digit > 0) {
+            while(!exit) {
+                if (isUnique(temp, digit%10, (digit/10)%10, (digit/100)%10, (digit/1000)%10, digit/10000)) {
+                    digit += temp * pow(10, i-1);
+                    exit = 1;
+                }
+                else temp = (rand()%limit)+1;
+            }
+        }
+        exit = 0;
+    }
+
+    return digit;
+}
--- main.c
+++ main.c
@@ -1,12 +1,16 @@
 #include "functions.h"
+#define PL 5
 
 int main(void) {
 
     int length = 10, height = 10;
+    srand(time(0));
 
     printf("\n\n");
     printBoard(length, height);
     printf("\n\n");
 
+    printf("\tTest roll: %d \n\n", playerSequence(PL));
+    
     return 0;
 }
