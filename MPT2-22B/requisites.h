2eacf59bce380bbf5bcfa1986bfd715aa5b16bbf
nzwh <ivanisaiahserato@gmail.com>
2022-05-17 20:38:32 +0800

    New: requisities.h
    Added a new file to contain some definitions and constants.

---
+++ MPT2-22B/requisites.h
@@ -0,0 +1,13 @@
+   
+    #define RLEN 7
+    #define CLEN 5
+    #define YLIM 2
+    #define ELIM 6
+
+    typedef struct intp {
+        int x; int y;
+    } intp;
+
+    typedef enum { 
+        false = 0, true = !false
+    } bool;
