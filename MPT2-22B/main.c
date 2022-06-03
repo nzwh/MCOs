bba6b176f3b5cc1af02ff4564a1d0f5cd0eab00f
nzwh <ivanisaiahserato@gmail.com>
2022-06-03 19:59:19 +0800

    Refactor Main
    Added user input instead of random, and refactored some sections.

--- MPT2-22B/main.c
+++ MPT2-22B/main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <time.h>
 
-#include "requisites.h"
+#include "constants.h"
 #include "functions.h"
+#include "graphics.h"
 
 int main(void) {
 
-    srand(time(0));
+    system("cls");
 
     ints base;
     Fill(&base);
@@ -16,40 +16,43 @@ int main(void) {
     bool aTurn = true, over = false, ok = false;
     base.a_length = base.e_length, base.b_length = base.y_length;
 
-    intp* Alpha = malloc(sizeof(intp) * base.p_length);
-    intp* Beta = malloc(sizeof(intp) * base.p_length);
+    intp *Alpha = malloc(sizeof(intp) * base.p_length);
+    intp *Beta = malloc(sizeof(intp) * base.p_length);
     Alpha = base.arr_e, Beta = base.arr_y;
 
     base.f_length = base.p_length - (base.a_length + base.b_length);
-    intp* Free = malloc(sizeof(intp) * base.f_length); 
+    intp *Free = malloc(sizeof(intp) * base.f_length);
     UpdateFree(Alpha, Beta, Free, &base);
 
+    base.m_length = base.a_length + base.b_length;
+    intp* Merge = malloc(sizeof(intp) * base.m_length);
+    MergeFill(Alpha, Beta, Merge, &base);
+
+    intp prev, next;
     char result;
-    intp prev = (intp){0, 0}, next;
 
-    for (int i = 1; !over; i++) {
-        
-        printf("\nRound %02d\n", i);
-        intp next = base.arr_s[rand() % base.s_length];
+    while (!over) {
 
-        printf("Previous: [%d, %d]\n", prev.x, prev.y);
-        printf("Next: [%d, %d]\n\n", next.x, next.y);
+        PrintBoard(RLEN, CLEN, Merge);
         fflush(stdout);
 
-        printf("Turn: %s", aTurn ? "Alpha\n" : "Beta\n");
-        fflush(stdout);
+        if (aTurn)  printf(LBLU "Player A's turn.\n" KRST);
+        else        printf(LMAG "Player B's turn.\n" KRST);
 
-        PrintInline(Alpha, base.a_length, "Alpha: ");
-        PrintInline(Beta, base.b_length, "Beta: ");
-        fflush(stdout);
+        printf("Choose a piece to move: ");
+        scanf("%d %d", &prev.x, &prev.y);
+        printf("Where do you want to place it: ");
+        scanf("%d %d", &next.x, &next.y);
 
         NextPlayerMove(prev, next, Alpha, Beta, Free, &base, &aTurn, &ok);
-        UpdateFree(Alpha, Beta, Free, &base); 
+        UpdateFree(Alpha, Beta, Free, &base);
+        MergeFill(Alpha, Beta, Merge, &base);
+
+        system("cls");
         result = GameOver(&over, Alpha, Beta, base);
-        prev = next;
     }
 
-    printf("\nWinner: Player %c !\n", result);
+    printf("\nWinner: %s Player %c !\n", result == 'A' ? LBLU : LMAG, result);
     Freer(Alpha, Beta, Free, &base);
 
     return 0;
