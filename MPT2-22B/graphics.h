a4e99e740134de979d11d4f0bb5a8b6ef3e32840
nzwh <ivanisaiahserato@gmail.com>
2022-06-16 06:28:45 +0800

    Better board
    Added two columns, one for the pieces, one for the coordinates. Also added other visual improvements.

--- MPT2-22B/graphics.h
+++ MPT2-22B/graphics.h
@@ -1,36 +1,67 @@
 
-    void PrintBoard(int r, int c, intp* Pieces) {
-        
-        char* top_div = NBMU NHFT NHFT NHFT NHFT NHFT NHFT NHFT NBMU NHFT NHFT NHFT NHFT NHFT NHFT NHFT NBMU NHFT NHFT NHFT NHFT NHFT NHFT NHFT NBMU NHFT NHFT NHFT NHFT NHFT NHFT NHFT NBMU NHFT NHFT NHFT NHFT NHFT NHFT NHFT NBMU;
-        char* mid_div = NBMR NHFT NHFT NHFT NHFT NHFT NHFT NHFT NALL NHFT NHFT NHFT NHFT NHFT NHFT NHFT NALL NHFT NHFT NHFT NHFT NHFT NHFT NHFT NALL NHFT NHFT NHFT NHFT NHFT NHFT NHFT NALL NHFT NHFT NHFT NHFT NHFT NHFT NHFT NBML;
-        char* bot_div = NBMD NHFT NHFT NHFT NHFT NHFT NHFT NHFT NBMD NHFT NHFT NHFT NHFT NHFT NHFT NHFT NBMD NHFT NHFT NHFT NHFT NHFT NHFT NHFT NBMD NHFT NHFT NHFT NHFT NHFT NHFT NHFT NBMD NHFT NHFT NHFT NHFT NHFT NHFT NHFT NBMD;
-        char* pipe = NVFT;
+    void PrintDivider(char* edge, char* fill, int length, int iter, int nl) {
 
-        int pad = 0; // 80/2 - ((int)(41/2));
-        printf("\n\n%*s%s\n", pad, "", top_div);
-        for (int i = 0, ctr = 0; i < r; i++) {
-
-            printf("%*s", pad, "");
-            printf("%s", pipe);
-            for (int j = 0; j < c; j++) {
-
-                if (Pieces[ctr].x == i + 1 && Pieces[ctr].y == j + 1) {
-
-                    if (Pieces[ctr++].p == 0) {
-                        printf("%s [%d,%d] %s", LBLU, i + 1, j + 1, KRST);
-                    } else {
-                        printf("%s [%d,%d] %s", LMAG, i + 1, j + 1, KRST);
-                    }
-
-                } else {
-                    printf(" [%d,%d] ", i + 1, j + 1);
-                }
-
-                printf("%s", pipe);
-            }
-
-            if (i + 1 < r) printf("\n%*s%s\n", pad, "", mid_div);
+        for (int i = 0; i < iter; i++) {
+            printf("%s", edge);
+            for (int i = 0; i < length; i++) 
+                printf("%s", fill);
         }
 
-        printf("\n%*s%s\n", pad, "", bot_div);
+        printf("%s", edge);
+        printf(nl ? "\n" : "   ");
+    }
+
+    void PrintBoard(int r, int c, intp* Pieces, int p_length) {
+
+        // int pad = 0; // 80/2 - ((int)(41/2));
+        // printf("\n\n%*s%s\n", pad, "", top_div);
+
+        PrintDivider(NBMU, NHFT, 3, CLEN, false);
+        PrintDivider(NBMU, NHFT, 7, CLEN, true);
+
+        for (int i = 0, ctr = 0; i < r; i++) {
+
+            for (int x = 0; x < 2; x++) {
+            for (int j = 0; j < c; j++) {
+
+                printf("%s", NVFT);
+                if (ctr < p_length && Pieces[ctr].x == i + 1 && Pieces[ctr].y == j + 1) {
+
+                    if (Pieces[ctr].p == 0) {
+
+                        x ?
+                        printf("%s [%d,%d] %s", LBLU, i + 1, j + 1, KRST) :
+                        printf("%s A %s", LBLU, KRST);
+
+                    } else if (Pieces[ctr].p == 1) {
+
+                        x ?
+                        printf("%s [%d,%d] %s", LMAG, i + 1, j + 1, KRST) :
+                        printf("%s B %s", LMAG, KRST);
+                    }
+
+                    ctr++;
+
+                } else {
+
+                    x ?
+                    printf(" [%d,%d] ", i + 1, j + 1) :
+                    (j + 1) % 2 == (i + 1) % 2 ? 
+                    printf(" - ") : printf("   ");
+                }
+            }
+
+            printf("%s   ", NVFT);
+            }
+
+            printf("\n");
+            if (i + 1 < r) {
+                PrintDivider(NALL, NHFT, 3, CLEN, false);
+                PrintDivider(NALL, NHFT, 7, CLEN, true);
+            }
+
+        }
+
+        PrintDivider(NBMD, NHFT, 3, CLEN, false);
+        PrintDivider(NBMD, NHFT, 7, CLEN, true);
 }
